5911c4d8c79e566036df1a8c177c5cd26efcee1e
John Manning <john.manning@microtraffic.com>
2022-07-04 21:41:53 -0500

    removed images from data

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,20 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-// import corwyn_trin_twitch_battlegame from "../data/corwyn_trin_twitch_battlegame.jpg";
-// import genet_igorna_duel from "../data/genet_igorna_duel.jpg";
-// import goblin_luck_duel from "../data/goblin_luck_duel.jpg";
-// import lip_twitch_duel from "../data/lip_twitch_duel.jpg";
-// import luck_duel from "../data/luck_duel.jpg";
-// import luck_javelin from "../data/luck_javelin.jpg";
-// import thistle_aera_battlegame from "../data/thistle_aera_battlegame.jpg";
-// import thistle_corwyn_twitch_battlegame from "../data/thistle_corwyn_twitch_battlegame.jpg";
-// import thistle_cycla_duel from "../data/thistle_cycla_duel.jpg";
-// import thistle_jump from "../data/thistle_jump.jpg";
-// import twitch_chelli_javelin from "../data/twitch_chelli_javelin.jpg";
-// import twitch_flail from "../data/twitch_flail.jpg";
-// import twitch_rabbit_jump from "../data/twitch_rabbit_jump.jpg";
-
 let Images = styled.ul`
   display: flex;
   flex-direction: column;
